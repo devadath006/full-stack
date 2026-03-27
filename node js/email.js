9d06d5229d290dd4e60e1f3938d314b92c100dd0
devadath006 <devadathme@gmail.com>
2026-03-27 20:56:37 +0530

    commit

---
+++ node js/email.js
@@ -0,0 +1,24 @@
+const nodemailer=require('nodemailer');
+const transporter=nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'devadathme@gmail.com',
+        pass:'vycmxpmnoeyhivcr'
+    }
+});
+
+const mailOptions={
+    from:'devadathme@gmail.com',
+    to:'devadathme@gmail.com',
+    subject:'testing',
+    text:'This email is sent using Node.js Nodemailer service.'
+};
+
+transporter.sendMail(mailOptions,(error,info)=>{
+    if(error){
+        console.log("Error occurred:",error);
+    }else{
+        console.log("Email sent succesfully!");
+        console.log("Response:",info.response);
+    }
+});
